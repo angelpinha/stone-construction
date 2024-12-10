5f8187e6534c14190e39660952557af7527bf1c0
Angel Piña <inbox.angelpinha@gmail.com>
2024-12-10 17:13:54 -0400

    pre daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				accentYellow: "#fca311",
-				laborBlue: "#14213d",
-				wallGray: "#e5e5e5",
-			}
+				stoneyellow: "#fca311",
+				stoneblue: "#14213d",
+				stonegray: "#e5e5e5",
+			},
+			fontFamily: {
+				sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+				main: ['Bebas Neue'],
+			},
 		},
 	},
 	plugins: [],
